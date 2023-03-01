af9f34580b19c7eb89a762d5ec8866784ebde32c
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-03-01 04:40:10 -0500

    Create elevator.py

---
+++ elevator.py
@@ -0,0 +1,15 @@
+#creating a python code for an elevator lets see how this goes
+floor = input('what floor are you going to? ')
+floor1 = '1'
+floor2 = '2'
+floor3 = '3'
+floor4 = '4'
+floor5 = '5'
+floor6 = '6'
+floor7 = '7'
+floor8 = '8'
+
+if floor == '1':
+  print ('going to ' + floor1 )
+elif floor > '1':
+  input('would you like to go to anyother floor')
